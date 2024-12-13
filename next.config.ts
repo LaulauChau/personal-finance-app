d02689d9a047d3c0508e13172014dcd4aa160373
Laurent Chau <mplaurent.chau@gmail.com>
2024-12-13 20:39:27 +0100

    chore: project initialized

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "~/config/env";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
